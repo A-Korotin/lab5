eb8f4314e5baa92b8b49bc25328bd326a3a37020
Alexey Korotin <sx12fx@gmail.com>
2022-02-18 12:44:03 +0300

    добавлена функция валидации данных при чтении файла

--- src/PropertiesRequester.java
+++ src/PropertiesRequester.java
@@ -1,3 +1,5 @@
+import javax.management.relation.RoleInfoNotFoundException;
+import java.util.List;
 import java.util.Scanner;
 
 public interface PropertiesRequester {
@@ -14,6 +16,34 @@ class DragonProperties {
     DragonCharacter character;
     double depth;
     Integer numberOfTreasures;
+
+    public static DragonProperties parseProperties(List<String> input, int indexShift){
+        DragonProperties properties = new DragonProperties();
+        if (input.size() != 9 + indexShift)
+            throw new RuntimeException("ОШИБКА! Неверное количество параметров");
+        try {
+            properties.name = input.get(0 + indexShift);
+            properties.xCoord = Float.parseFloat(input.get(1 + indexShift));
+            properties.yCoord = Integer.parseInt(input.get(2 + indexShift));
+            properties.age = Long.parseLong(input.get(3 + indexShift));
+            properties.color = Color.valueOf(input.get(4 + indexShift));
+            properties.type = DragonType.valueOf(input.get(5 + indexShift));
+            properties.character = DragonCharacter.valueOf(input.get(6 + indexShift));
+            properties.depth = Double.parseDouble(input.get(7 + indexShift));
+            properties.numberOfTreasures = Integer.parseInt(input.get(8 + indexShift));
+        } catch (RuntimeException e) {
+            throw new RuntimeException("ОШИБКА! Типы данных несовместимы");
+        }
+        if (properties.name.isEmpty())
+            throw new RuntimeException("ОШИБКА! Параметр ИМЯ не может быть пустым");
+        if (properties.yCoord > 998)
+            throw new RuntimeException("ОШИБКА! Параметр КООРДИНАТА_Y не может быть >998");
+        if (properties.age <= 0)
+            throw new RuntimeException("ОШИБКА !Параметр ВОЗРАСТ не может быть <= 0");
+        if (properties.numberOfTreasures <= 0)
+            throw new RuntimeException("ОШИБКА! Параметр КОЛИЧЕСТВО_СОКРОВИЩ_В_ПЕЩЕРЕ не может быть <= 0");
+        return properties;
+    }
 }
 
 class ConsoleRequester implements PropertiesRequester {
