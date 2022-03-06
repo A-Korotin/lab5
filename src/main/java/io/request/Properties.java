fe2eba3e4ce0d6c364dd8d3d12494705d181abd7
Alexey Korotin <sx12fx@gmail.com>
2022-03-07 00:36:29 +0300

    !!! Нерабочая версия !!! Добавлен статический метод parseProperties

--- src/main/java/io/request/Properties.java
+++ src/main/java/io/request/Properties.java
@@ -3,6 +3,7 @@ package io.request;
 import dragon.Color;
 import dragon.DragonCharacter;
 import dragon.DragonType;
+import java.util.List;
 
 /**
 * Класс присваивания полям элементов значений, полученных с консоли или из файла
@@ -17,33 +18,39 @@ public class Properties {
     public DragonCharacter character;
     public double depth;
     public Integer numberOfTreasures;
+
+    public static Properties parseProperties(List<String> input, int indexShift) throws Exception {
+        if (input.size() != 9 + indexShift)
+            throw new Exception(String.format("Ошибка. Неверное количество параметров. Ожидалось %d, получено %d", 9 + indexShift, input.size()));
+
+        Properties properties = new Properties();
+
+        try {
+            properties.name = input.get(0 + indexShift);
+            properties.xCoord = Float.parseFloat(input.get(1 + indexShift));
+            properties.yCoord = Integer.parseInt(input.get(2 + indexShift));
+            properties.age = Long.parseLong(input.get(3 + indexShift));
+            properties.color = input.get(4 + indexShift).equals("null") ? null: Color.valueOf(input.get(4 + indexShift).toUpperCase());
+            properties.type = DragonType.valueOf(input.get(5 + indexShift).toUpperCase());
+            properties.character = input.get(6 + indexShift).equals("null") ? null : DragonCharacter.valueOf(input.get(6 + indexShift).toUpperCase());
+            properties.depth = Double.parseDouble(input.get(7 + indexShift));
+            properties.numberOfTreasures = input.get(8 + indexShift).equals("null") ? null: Integer.parseInt(input.get(8 + indexShift));
+        } catch (RuntimeException e) {
+            throw new Exception("Ошибка. Типы данных несовместимы.");
+        }
+
+        if (properties.name.isEmpty())
+            throw new Exception("Ошибка. Параметр ИМЯ не может быть пустым");
+
+        if (properties.yCoord > 998)
+            throw new Exception("Ошибка. Параметр КООРДИНАТА_Y не может быть >998");
+
+        if (properties.age <= 0)
+            throw new Exception("Ошибка. Параметр ВОЗРАСТ не может быть <= 0");
+
+        if (properties.numberOfTreasures != null && properties.numberOfTreasures <= 0)
+            throw new Exception("Ошибка. Параметр КОЛИЧЕСТВО_СОКРОВИЩ_В_ПЕЩЕРЕ не может быть <= 0");
+
+        return properties;
+    }
 }
-//
-//
-//    public static DragonProperties parseProperties(List<String> input, int indexShift){
-//        DragonProperties properties = new DragonProperties();
-//        if (input.size() != 9 + indexShift)
-//            throw new RuntimeException("ОШИБКА! Неверное количество параметров");
-//        try {
-//            properties.name = input.get(0+indexShift);
-//            properties.xCoord = Float.parseFloat(input.get(1 + indexShift));
-//            properties.yCoord = Integer.parseInt(input.get(2 + indexShift));
-//            properties.age = Long.parseLong(input.get(3 + indexShift));
-//            properties.color = input.get(4 + indexShift).equals("null") ? null: Color.valueOf(input.get(4 + indexShift));
-//            properties.type = DragonType.valueOf(input.get(5 + indexShift));
-//            properties.character = input.get(6 + indexShift).equals("null") ? null : DragonCharacter.valueOf(input.get(6 + indexShift));
-//            properties.depth = Double.parseDouble(input.get(7 + indexShift));
-//            properties.numberOfTreasures = input.get(8 + indexShift).equals("null") ? null: Integer.parseInt(input.get(8 + indexShift));
-//        } catch (RuntimeException e) {
-//            throw new RuntimeException("ОШИБКА! Типы данных несовместимы");
-//        }
-//        if (properties.name.isEmpty()) // redundant
-//            throw new RuntimeException("ОШИБКА! Параметр ИМЯ не может быть пустым");
-//        if (properties.yCoord > 998)
-//            throw new RuntimeException("ОШИБКА! Параметр КООРДИНАТА_Y не может быть >998");
-//        if (properties.age <= 0)
-//            throw new RuntimeException("ОШИБКА !Параметр ВОЗРАСТ не может быть <= 0");
-//        if (properties.numberOfTreasures != null && properties.numberOfTreasures <= 0)
-//            throw new RuntimeException("ОШИБКА! Параметр КОЛИЧЕСТВО_СОКРОВИЩ_В_ПЕЩЕРЕ не может быть <= 0");
-//        return properties;
-//    }
---
+++ src/test/java/io/request/PropertiesTest.java
@@ -0,0 +1,37 @@
+package io.request;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PropertiesTest {
+
+    private List<String> in;
+
+    @Test
+    void parseProperties() {
+        in = new ArrayList<String>(
+                List.of(new String[]{
+                        "name",
+                        "213.4",
+                        "97",
+                        "213",
+                        "null",
+                        "fire",
+                        "good",
+                        "213",
+                        "231"
+                })
+        );
+        try {
+            Properties p = Properties.parseProperties(in, 0);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            assert false;
+        }
+    }
+}
