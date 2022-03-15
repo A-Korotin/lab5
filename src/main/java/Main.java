056aa5dd9a776735fe3f8ecc3f6f87bef40fa044
Alexey Korotin <sx12fx@gmail.com>
2022-03-15 12:30:48 +0300

    !!! Нерабочая версия !!!

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,16 @@
 import collection.DAO;
+import collection.Describable;
+import collection.DragonDAO;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import io.*;
 import io.request.*;
 
+import java.io.File;
 
 public class Main {
     public static void main(String[] args) throws JsonProcessingException {
         DAO dao = new FileManipulator().get();
+
         ConsoleRequester requester = new ConsoleRequester();
         Properties p = requester.requestProperties();
         dao.create(p);
