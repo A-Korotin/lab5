0cdac658e3cc0ad3841baf06b6c33d3fc0107f6b
Alexey Korotin <sx12fx@gmail.com>
2022-04-04 18:13:08 +0300

    Добавлен функционал клиента

--- src/main/java/net/Client.java
+++ src/main/java/net/Client.java
@@ -1,88 +1,56 @@
 package net;
-import commands.Command;
-import commands.CommandCreator;
-import commands.ExecuteScript;
-import commands.dependencies.Instances;
-import io.ConsoleOutput;
-import io.ConsoleReader;
-import io.FileReader;
-import io.request.ConsoleRequester;
+
+import exceptions.ResponseTimeoutException;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
-import java.net.SocketAddress;
-import java.nio.*;
-import java.nio.channels.ClosedChannelException;
+import java.nio.ByteBuffer;
 import java.nio.channels.DatagramChannel;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.charset.StandardCharsets;
-import java.util.List;
 
 public final class Client {
-    private DatagramChannel channel;
-    private SocketAddress address;
-    private Selector selector;
-    private ByteBuffer buffer;
-    private Instances instances;
+    private final Selector selector;
 
     public Client(String host, int port) throws IOException {
-        address = new InetSocketAddress(host, port);
-        buffer = ByteBuffer.allocate(1024);
+        InetSocketAddress address = new InetSocketAddress(host, port);
+        DatagramChannel channel = DatagramChannel.open().connect(address);
         selector = Selector.open();
-        channel = DatagramChannel.open();
         channel.configureBlocking(false);
-
-        registerWrite();
-
-        instances = new Instances();
-        instances.outPutter = new ConsoleOutput();
-        instances.fileReader = new FileReader();
-        instances.consoleReader = new ConsoleReader();
-        instances.consoleRequester = new ConsoleRequester();
+        channel.register(selector, SelectionKey.OP_WRITE, address);
     }
 
-    private void registerRead() throws ClosedChannelException {
-        channel.register(selector, SelectionKey.OP_READ);
-    }
-
-    private void registerWrite() throws ClosedChannelException {
-        channel.register(selector, SelectionKey.OP_WRITE);
-    }
-
-
-    private List<Command> getUserInput() {
-        List<Command> list = CommandCreator.getCommands(instances.consoleReader);
-        Command command = list.get(0);
-        return command instanceof ExecuteScript ?
-                ((ExecuteScript) command).getNestedCommands(instances) : list;
-    }
-
-    private void loopBody() throws IOException {
+    public String sendAndReceiveResponse(String message, long timeout) throws IOException {
         selector.select();
-
-        while(Time);
+        for (SelectionKey key: selector.selectedKeys()) {
+            if(key.isWritable()) {
+                write(key, message);
+                return read(key, timeout);
+            }
+        }
+        return null;
     }
 
-
-    public void sendMsg(String msg) throws IOException {
-        channel.send(ByteBuffer.wrap(msg.getBytes(StandardCharsets.UTF_16)), address);
+    private void write(SelectionKey key, String msg) throws IOException {
+        DatagramChannel channel = (DatagramChannel) key.channel();
+        channel.write(StandardCharsets.UTF_16.encode(msg));
     }
 
-    public String receiveMsg() throws IOException {
+    private String read(SelectionKey key, long timeout) throws IOException {
+        DatagramChannel channel = (DatagramChannel) key.channel();
+        ByteBuffer buffer = ByteBuffer.allocate(4096);
         buffer.clear();
-        address = channel.receive(buffer);
-        buffer.flip();
-        byte[] bytes = new byte[buffer.remaining()];
-        buffer.get(bytes);
-        return new String(bytes, StandardCharsets.UTF_16);
-    }
+        long startTime = System.currentTimeMillis();
 
-    public static void main(String[] args) throws IOException {
-        Client client = new Client("localhost", 4444);
+        while (System.currentTimeMillis() - startTime < timeout * 1000) {
 
-        client.sendMsg("Hello");
-        //client.buffer.flip();
-        System.out.println("Ответ: " + client.receiveMsg());
+            if (channel.read(buffer) == 0) continue;
+
+            buffer.flip();
+            return StandardCharsets.UTF_16.decode(buffer).toString();
+        }
+
+        throw new ResponseTimeoutException();
     }
 }
